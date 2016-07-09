d24d2d9a40601f0f43df06a81302bfcfeafe104d
schaible <sarah.schaible@gmail.com>
2016-07-09 18:04:02 -0400

    fizzbuzz with classes

---
+++ Fizzbuzz_Class/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz_Class
+{
+    public class Fizz_Buzz
+    {
+
+        public string[] callout1 = { "fizz", "fuzz", "fizzy", "fuzzy" };
+        public string[] callout2 = { "buzz", "bizz", "buzzy", "bizzy" };
+        public string[] callout3 = { "fizz buzz", "fuzzy bizz", "bizzy fuzzy", "fizzy bizzy" };
+
+        
+        public void Do_Math()
+        {
+            for (int number = 0; number <= 200; number++)
+            {
+                if ((number % 3 == 0) && (number % 5 == 0)) 
+                {
+                    Console.WriteLine(callout3[Randomize()]);
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine(callout1[Randomize()]);
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine(callout2[Randomize()]);
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+                number += 1;
+
+            }
+
+
+        }
+
+        public int Randomize()
+        {
+            Random rnd = new Random(); 
+            return rnd.Next(4);
+        }
+
+
+
+
+        
+
+    }
+}
---
+++ fizzbuzz_rebuild_with_class/Program.cs
@@ -0,0 +1,20 @@
+﻿using Fizzbuzz_Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz_rebuild_with_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fizz_Buzz project = new Fizz_Buzz();
+            
+            project.Do_Math();
+            Console.ReadLine();
+        }
+    }
+}
